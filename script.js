6d2681ac1dffd72663191d06f26fa2ec1d425df2
Ayush Singh <ayush5292singh@gmail.com>
2026-07-24 15:42:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function speak(){
 
+let text =
+document.getElementById("message").innerText;
+
+
+let speech =
+new SpeechSynthesisUtterance(text);
+
+
+speech.rate = 1;
+
+speech.pitch = 1.2;
+
+
+window.speechSynthesis.speak(speech);
+
+}
