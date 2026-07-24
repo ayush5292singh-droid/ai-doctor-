5917b296f7fdfe14a49995dc40a5f61937914ad4
Ayush Singh <ayush5292singh@gmail.com>
2026-07-24 15:44:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,87 @@ speech.pitch = 1.2;
 window.speechSynthesis.speak(speech);
 
 }
+let medicines = [];
+
+function addMedicine(){
+
+let name =
+document.getElementById("medicineName").value;
+
+let time =
+document.getElementById("medicineTime").value;
+
+
+if(name==="" || time===""){
+
+alert("Enter medicine details");
+
+return;
+
+}
+
+
+medicines.push({
+name:name,
+time:time
+});
+
+
+showMedicines();
+
+document.getElementById("medicineName").value="";
+document.getElementById("medicineTime").value="";
+
+}
+
+
+
+function showMedicines(){
+
+let list="";
+
+
+medicines.forEach(function(medicine,index){
+
+list +=
+
+"<p>💊 "+medicine.name+
+" ⏰ "+medicine.time+
+" <button onclick='deleteMedicine("+index+")'>❌</button></p>";
+
+});
+
+
+document.getElementById("medicineList").innerHTML=list;
+
+}
+
+
+
+function deleteMedicine(index){
+
+medicines.splice(index,1);
+
+showMedicines();
+
+}
+
+
+
+let waterCount=0;
+
+
+function addWater(){
+
+if(waterCount<8){
+
+waterCount++;
+
+}
+
+
+document.getElementById("water").innerHTML=
+
+waterCount+" / 8 glasses";
+
+}
